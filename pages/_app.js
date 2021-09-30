f867aabfb56235bbf34418919cb7171e6b6dc8ab
Tomkhcoding <tom.kodehode@gmail.com>
2021-09-30 18:26:31 +0200

    styled components maybe

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,35 @@
-import '../styles/globals.css'
+import { createGlobalStyle, ThemeProvider } from "styled-components"
+
+const GlobalStyle = createGlobalStyle`
+  html, body {
+    padding: 0;
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+      Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+    background-color: #202020; 
+    color: #f5f5f5;
+  }
+
+  a {
+  color: inherit;
+  text-decoration: none;
+  }
+
+  * {
+  box-sizing: border-box;
+  }
+
+`
+
+
+const theme = {}
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return 
+  <>
+    <GlobalStyle />
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  </>
 }
--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,62 @@
+import styled from 'styled-components'
+
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+
+const Container = styled.div`
+  min-height: 100vh;
+  padding: 0 0.5rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`
+
+const Main = styled.div`
+  padding: 5rem 0;
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+
+const Footer = styled.div`
+  width: 100%;
+  height: 100px;
+  border-top: 1px solid #eaeaea;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+    a {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      flex-grow: 1;
+    }
+`
+
+
 
 export default function Home() {
+
+
   return (
-    <div className={styles.container}>
+    <Container>
       <Head>
         <title>GIMP Info</title>
         <meta name="description" content="Created by Echo" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
+      <Main>
         <h1>Names of the degens</h1>
         <h3>GIMFunFun</h3>
         <h3>emerald12 gim</h3>
-      </main>
+      </Main>
 
-      <footer className={styles.footer}>
+      <Footer>
         <a
           href="https://github.com/KodeAndre"
           target="_blank"
@@ -25,7 +64,7 @@ export default function Home() {
         >
           Powered by Echo
         </a>
-      </footer>
-    </div>
+      </Footer>
+    </Container>
   )
 }
