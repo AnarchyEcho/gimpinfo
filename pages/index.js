d3fe41bdcd140d5e8e66079effa4219ddd240e4b
kodeandre <andre.kodehode@gmail.com>
2021-09-30 16:44:45 +0200

    Feat: Darkmode

--- pages/index.js
+++ pages/index.js
@@ -58,10 +58,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
+          Powered by Echo
         </a>
       </footer>
     </div>
