ba3ae6914044205faf6320973cc6f24300c74e6d
KodeAndre <andre.kodehode@gmail.com>
2021-10-04 12:55:52 +0200

    Feat: quests list, no functionality

--- pages/edit.js
+++ pages/edit.js
@@ -1,8 +1,23 @@
 import { useSession, signIn, signOut } from "next-auth/client";
 import Link from 'next/link'
+import Head from 'next/Head'
+import { QuestTool } from "osrs-quest-tool";
 
 export default function Edit() {
   const [session, loading] = useSession()
+
+  const tool = new QuestTool();
+  const toolJson = JSON.parse(JSON.stringify(tool));
+  const quests = Object.keys(toolJson.questObject);
+
+
+  const Header = 
+        <Head>
+          <title>GIMP Edit</title>
+          <meta name="description" content="Created by Echo and Tom" />
+          <link rel="icon" href="/GimIconTransparent.png" />
+          <meta property="og:image" content="/GimIconTransparent.png" />
+        </Head>
 
   const Login =
         <div>
@@ -22,6 +37,7 @@ export default function Edit() {
   if (loading) {
     return (
     <div>
+      {Header}
       <p>Loading...</p>
     </div>
     )
@@ -30,6 +46,7 @@ export default function Edit() {
   if (!session) {
     return (
       <div>
+        {Header}
         <Link href="/">Home</Link>
         <p>You are not logged in.</p>
         {Login}
@@ -39,9 +56,15 @@ export default function Edit() {
 
   return (
     <>
+      {Header}
       <Link href="/">Home</Link>
       <p>You are logged in</p>
       {Login}
+      {quests.map( x =>
+        <div key={x}>
+            <p>{x}</p>
+        </div>
+      )}
     </>
   )
 }
