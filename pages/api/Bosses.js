257a75493d449f0a927d22ef3f123db2023df3c4
Tom <tom.kodehode@gmail.com>
2021-10-08 12:31:13 +0200

    feat: boss api

---
+++ pages/api/Bosses.js
@@ -0,0 +1,191 @@
+export default function handler(req, res) {
+    res.status(200).json({ 
+        abyssalSire : {
+            picture :"/bosses/AbyssalOrphan.png",
+            name : "Abyssal sire"
+        },
+        alchemicalHydra : {
+            picture :"/bosses/IkkleHydra.png",
+            name : "Alchemical Hydra"
+        },
+        barrowsChests : {
+            picture :"/bosses/Chestbarrows.png",
+            name : "Barrows"
+        },
+        bryophyta : {
+            picture :"/bosses/Bryophytasessence.png",
+            name : "Bryophyta"
+        },
+        callisto : {
+            picture :"/bosses/Callistocub.png",
+            name : "Callisto"
+        },
+        cerberus : {
+            picture :"/bosses/Hellpuppy.png",
+            name : "Cerberus"
+        },
+        chambersOfXeric : {
+            picture :"/bosses/Olmlet.png",
+            name : "CoX"
+        },
+        chambersOfXericChallengeMode : {
+            picture :"/bosses/Metamorphicdust.png",
+            name : "CoX CM"
+        },
+        chaosElemental : {
+            picture :"/bosses/Petchaoselemental.png",
+            name : "Chaos Elemental"
+        },
+        chaosFanatic : {
+            picture :"/bosses/Odiumshard1.png",
+            name : "Chaos Fanatic"
+        },
+        commanderZilyana : {
+            picture :"/bosses/Petzilyana.png",
+            name : "Commander Zilyana"
+        },
+        corporalBeast : {
+            picture :"/bosses/petcorporealcritter.png",
+            name : "Corporeal Beast"
+        },
+        crazyArcheologist : {
+            picture :"/bosses/Fedora.png",
+            name : "Crazy Archaeologist"
+        },
+        dagannothPrime : {
+            picture :"/bosses/Petdagannothprime.png",
+            name : "Dagannoth Prime"
+        },
+        dagannothRex : {
+            picture :"/bosses/Petdagannothrex.png",
+            name : "Dagannoth Rex"
+        },
+        dagannothSupreme : {
+            picture :"/bosses/Petdagannothsupreme.png",
+            name : "Dagannoth Supreme"
+        },
+        derangedArchaeologist : {
+            picture :"/bosses/Wintertodtparable.png",
+            name : "Deranged Archaeologist"
+        },
+        generalGraardor : {
+            picture :"/bosses/Petgeneralgraardor.png",
+            name : "General Graardor"
+        },
+        giantMole : {
+            picture :"/bosses/Baby mole.png",
+            name : "GiantMole"
+        },
+        grotesqueGuardians : {
+            picture :"/bosses/Noon.png",
+            name : "Grotesque Guardians"
+        },
+        hespori : {
+            picture :"/bosses/Bottomlesscompostbucket.png",
+            name : "Hespori"
+        },
+        kalphiteQueen : {
+            picture :"/bosses/Kalphiteprincess.png",
+            name : "Kalphite Queen"
+        },
+        kingBlackDragon : {
+            picture :"/bosses/Princeblackdragon.png",
+            name : "King Black Dragon"
+        },
+        kraken : {
+            picture :"/bosses/Petkraken.png",
+            name : "Kraken"
+        },
+        kreeArra : {
+            picture :"/bosses/Petkreearra.png",
+            name : "Kree'Arra"
+        },
+        krilTsutsaroth : {
+            picture :"/bosses/Petkriltsutsaroth.png",
+            name : "K'ril Tsutsaroth"
+        },
+        mimic : {
+            picture :"/bosses/The_Mimic.png",
+            name : "Mimic"
+        },
+        nightmare : {
+            picture :"/bosses/Littlenightmare.png",
+            name : "Nightmare"
+        },
+        phosanisNightmare : {
+            picture :"/bosses/Parasiticegg.png",
+            name : "Phosani's Nightmare"
+        },
+        obor : {
+            picture :"/bosses/Hillgiantclub.png",
+            name : "Obor"
+        },
+        sarachnis : {
+            picture :"/bosses/Sraracha.png",
+            name : "Sarachnis"
+        },
+        scorpia : {
+            picture :"/bosses/Scorpias offspring.png",
+            name : "Scorpia"
+        },
+        skotizo : {
+            picture :"/bosses/Skotos.png",
+            name : "Skotizo"
+        },
+        tempoross : {
+            picture :"/bosses/Tinytempor.png",
+            name : "Tempoross"
+        },
+        theGauntlet : {
+            picture :"/bosses/Youngllef.png",
+            name : "The Gauntel"
+        },
+        theCorruptedGauntlet : {
+            picture :"/bosses/Gauntletcape.png",
+            name : "The Corrupted Gauntlet"
+        },
+        theatreofBlood : {
+            picture :"/bosses/Lilzik.png",
+            name : "ToB"
+        },
+        theatreofBloodHard : {
+            picture :"/bosses/Sanguinedust.png",
+            name : "ToB Hardmode"
+        },
+        thermonuclearSmokeDevil : {
+            picture :"/bosses/Petsmokedevil.png",
+            name : "Thermonuclear Smoke Devil"
+        },
+        tzKalZuk : {
+            picture :"/bosses/tzrekzuk.png",
+            name : "Zuk"
+        },
+        tzTokJad : {
+            picture :"/bosses/tzrekjad.png",
+            name : "Jad"
+        },
+        venenatis : {
+            picture :"/bosses/Venenatisspiderling.png",
+            name : "Venenatis"
+        },
+        vetion : {
+            picture :"/bosses/Vetionjr.png",
+            name : "Vet'ion"
+        },
+        vorkath : {
+            picture :"/bosses/vorkathshead.png",
+            name : "Vorkath"
+        },
+        Wintertodt : {
+            picture :"/bosses/Phoenix.png",
+            name : "Wintertodt"
+        },
+        zalcano : {
+            picture :"/bosses/Smolcano.png",
+            name : "Zalcano"
+        },
+        zulrah : {
+            picture :"/bosses/snakeling.png",
+            name : "Zulrah"
+        }
+    })}
