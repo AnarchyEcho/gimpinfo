81c182d5774ea8ed3a5bc55bb0100a859674744d
KodeAndre <84140381+KodeAndre@users.noreply.github.com>
2021-10-12 09:46:41 +0200

    Update bank.js

--- pages/bank.js
+++ pages/bank.js
@@ -1,12 +1,13 @@
-import {Items} from 'oldschooljs'
+import { Items } from 'oldschooljs';
 
 
 const item = Items.get("twisted bow")
-console.log(items)
+console.log(item)
 export default function Bank() {
 
     return (
-        <>
-        </>
+        <div>
+            <p>lol</p>
+        </div>
     )
 }
